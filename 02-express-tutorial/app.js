692475d41293ba3242594d15da94b221be0762e6
Wesley Lo <weslo404@gmail.com>
2022-07-13 14:08:18 -0400

    setup static and middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -6,9 +6,11 @@ const app = express()
 // setup static and middleware
 app.use(express.static('./public'))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+//   adding to static assets
+//   SSR
+// })
 
 app.all('*', (req, res) => {
   res.status(404).send('resource not found')
