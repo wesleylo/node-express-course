534e6926b26f34fbeda7930b45ce3a2d5b4f26ac
Wesley Lo <weslo404@gmail.com>
2022-07-13 14:29:20 -0400

    update answers

--- 02-express-tutorial/answers/04-express-app.js
+++ 02-express-tutorial/answers/04-express-app.js
@@ -14,6 +14,7 @@ app.all('*', (req, res) => {
   res.status(404).send('resource not found')
 })
 
-app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
+//Error: listen EADDRINUSE: address already in use :::5000
+app.listen(5001, () => {
+  console.log('server is listening on port 5001....')
 })
