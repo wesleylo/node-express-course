6f1d3c42585eac6c393ed108824a1a0a284d3f6d
Wesley Lo <weslo404@gmail.com>
2022-07-13 20:51:40 -0400

    update app.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,22 +1,58 @@
 const express = require('express')
-const path = require('path')
-
 const app = express()
+const { products } = require('./data')
 
-// setup static and middleware
-app.use(express.static('./public'))
+app.get('/', (req, res) => {
+  res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
+})
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product
+    return { id, name, image }
+  })
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-//   adding to static assets
-//   SSR
-// })
+  res.json(newProducts)
+})
+app.get('/api/products/:productID', (req, res) => {
+  // console.log(req)
+  // console.log(req.params)
+  const { productID } = req.params
 
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  )
+  if (!singleProduct) {
+    return res.status(404).send('Product Does Not Exist')
+  }
+
+  return res.json(singleProduct)
 })
 
-//Error: listen EADDRINUSE: address already in use :::5000
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+  console.log(req.params)
+  res.send('hello world')
+})
+
+app.get('/api/v1/query', (req, res) => {
+  // console.log(req.query)
+  const { search, limit } = req.query
+  let sortedProducts = [...products]
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search)
+    })
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit))
+  }
+  if (sortedProducts.length < 1) {
+    // res.status(200).send('no products matched your search');
+    return res.status(200).json({ sucess: true, data: [] })
+  }
+  res.status(200).json(sortedProducts)
+})
+
 app.listen(5001, () => {
-  console.log('server is listening on port 5001....')
+  console.log('Server is listening on port 5001....')
 })
